69309921c11bc8672d7c716ee3387ae2e42df752
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-02 13:50:12 -0300

    feat(getitem): eu preciso identificar cada mensagem com um id

--- src/cookie/getItem.js
+++ src/cookie/getItem.js
@@ -2,7 +2,9 @@ import * as f from 'f'
 
 export default (key) =>
   new Promise(function (resolve) {
-    self.postMessage({ type: 'cookie:get', payload: { key } })
+    const id = new Date().getTime().toString(32)
+
+    self.postMessage({ type: 'cookie:get', payload: { id, key } })
     self.addEventListener('message', (e) =>
-      f.equal(e.data.type, 'cookie:get') && resolve(e.data.value))
+      f.and(f.equal(e.data.type, 'cookie:get'), f.equal(e.data.id, id)) && resolve(e.data.value))
   })
