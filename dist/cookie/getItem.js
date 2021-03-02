14e80f4d84416103b9397972e7dce66c9d14dc24
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-02 13:52:06 -0300

    build(dist): automatic new publish

--- dist/cookie/getItem.js
+++ dist/cookie/getItem.js
@@ -1,6 +1,5 @@
 import * as f from 'f';
 export default (key) => new Promise(function (resolve) {
-    const id = new Date().getTime().toString(32);
-    self.postMessage({ type: 'cookie:get', payload: { id, key } });
-    self.addEventListener('message', (e) => f.and(f.equal(e.data.type, 'cookie:get'), f.equal(e.data.id, id)) && resolve(e.data.value));
+    self.postMessage({ type: 'cookie:get', payload: { key } });
+    self.addEventListener('message', (e) => f.and(f.equal(e.data.type, 'cookie:get'), f.equal(e.data.key, key)) && resolve(e.data.value));
 });
